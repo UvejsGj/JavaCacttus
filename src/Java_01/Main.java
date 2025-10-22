11d10168f125de92a890f1e8eac7e347e665ecc3
Uvejs Gjelaj <61552503+UvejsGj@users.noreply.github.com>
2025-10-22 18:46:31 +0200

    hi

---
+++ src/Java_01/Main.java
@@ -0,0 +1,46 @@
+package Java_01;// emri i Klases duhet me fillu me shkronje te madhe
+// emri i Klases i njejt me emrin e file-it psh: Java_01.Main.java, klasa => public class Java_01.Main
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("=============================");
+        System.out.println("======= CV Resume ===========");
+        System.out.println("====== Emri: Uvejs ==========");
+        System.out.println("==== Mbiemri: Gjelaj ========");
+    }
+}
+
+    /*
+    public static void main(String[] args) {
+        System.out.println("Hello from Java");
+
+        // Syntax error
+        // System.out.printlnN("Gabim sintaksor");
+
+        // Runtime error
+        // System.out.println("Gabim ne runtime" + 1 / 0);
+
+        // Semantic ose Error logjik
+        // Shuma e dy numrave 2 + 2 na jep rezultatin 5 ose 3
+        // System.out.println("2 + 2 = 5 ??");
+
+        // Komentet
+        // Single Line
+
+
+        Shume
+        rreshta
+        koment
+
+
+    }
+}
+*/
+// .idea permban file-at konfigurues per IntelliJ
+// src Folder qe permban kodin tone
+// out Folder qe permban output-in e kodit/permban byte kodin pas compajllimit
+// Kur i kemi thon run IntellIj e ka ekzeukutu qet komanden "javac Java_01.Main.java"
+// Mandej u kriju ni file i ri ne follderin out e emrin Java_01.Main.class qe permban byte kodin
+// Byte kodi i Java_01.Main.class ekzekutohet me fjalen Kyce "java Java_01.Main"
---
+++ src/Java_02/detyreKlase.java
@@ -0,0 +1,50 @@
+package Java_02;
+
+public class detyreKlase {
+    public static void main(String[] args) {
+//        System.out.println("===============================");
+//        System.out.println("============ Fatura ===========");
+//        System.out.println("===============================");
+//        System.out.printf("Emri dhe mbiemri: %s %S%n", "Uvejs", "Gjelaj");
+//        System.out.printf("Produkti: %S%n", "Laptop");
+//        System.out.printf("Sasia: %d%n", 1);
+//        System.out.println("-------------------------------");
+//        System.out.printf("%20s: %.2f$%n", "Total",399.5);
+//        System.out.println("===============================");
+//
+//        System.out.println("---------------");
+//        System.out.println("|             |");
+//        System.out.println("|             |");
+//        System.out.println("|             |");
+//        System.out.println("|             |");
+//        System.out.println("|             |");
+//        System.out.println("|             |");
+//        System.out.println("|             |");
+//        System.out.println("_______________");
+//
+//        System.out.println("     J     a       v       v     a");
+//        System.out.println("     J    a  a      v     v     a  a");
+//        System.out.println("J    J   a a  a      v   v     a a  a");
+//        System.out.println(" J J J  a      a       v      a      a ");
+
+
+//        System.out.println("* * * * * * ========================");
+//        System.out.println(" * * * * *  ========================");
+//        System.out.println("* * * * * * ========================");
+//        System.out.println(" * * * * *  ========================");
+//        System.out.println("* * * * * * ========================");
+//        System.out.println(" * * * * *  ========================");
+//        System.out.println("====================================");
+//        System.out.println("====================================");
+//        System.out.println("====================================");
+//        System.out.println("====================================");
+//        System.out.println("====================================");
+
+//        System.out.println(" +'''''+ ");
+//        System.out.println("[| O O |]");
+//        System.out.println(" |  ^  | ");
+//        System.out.println(" |     | ");
+//        System.out.println(" | '-' | ");
+//        System.out.println(" +-----+ ");
+    }
+}
