2cca0074b71a3ee8c401eb61781f025c0b4c067c
Uvejs Gjelaj <61552503+UvejsGj@users.noreply.github.com>
2025-11-04 18:36:15 +0100

    Create StringExample.java

---
+++ src/Java_04/StringExample.java
@@ -0,0 +1,18 @@
+package Java_04;
+
+public class StringExample {
+    public static void main(String[] args) {
+//        String numri7 = "7";
+//        int numri2 = 2;
+//        System.out.println("72? " + (numri7 + numri2));
+
+        // String nuk eshte tip primitiv eshte tip komplex (klase_
+        char A = 'A';
+        char B = 'B';
+        String AB = "AB";
+
+        String text = "Ky eshte nje text";
+//        System.out.printf("Numri i shkronjave ne kete teskt eshte: " + text.length());
+        System.out.println("Gjatesia e tekstit: " + text.length());
+    }
+}
