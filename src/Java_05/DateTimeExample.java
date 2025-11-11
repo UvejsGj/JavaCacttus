9421fab484f75b3966277141c6865f94dfd9d9ef
Uvejs Gjelaj <61552503+UvejsGj@users.noreply.github.com>
2025-11-11 17:26:20 +0100

    DataTimeExample

---
+++ src/Java_05/DateTimeExample.java
@@ -0,0 +1,23 @@
+package Java_05;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.GregorianCalendar;
+
+public class DateTimeExample {
+    static void main(String[] args) {
+        // Nese doni me gjet daten dhe kohen aktuale te sistemit operativ
+        // Mundei m dy klasa
+        // per date: LocalDate
+        // per date dhe ore: LocalDateTime
+        LocalDate data = LocalDate.now();
+        System.out.println("Data: " + data);
+
+        LocalDateTime dataDheOra = LocalDateTime.now();
+        System.out.println("Data dhe ora: " + dataDheOra);
+
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        System.out.println("Gregorian Calendar: " + gregorianCalendar.getTime());
+        System.out.println("Gregorian timezone: " + gregorianCalendar.getTimeZone());
+    }
+}
