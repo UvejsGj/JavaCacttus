49b2e22318d30a052e79b4be7af8fc6bc27913ba
Uvejs Gjelaj <61552503+UvejsGj@users.noreply.github.com>
2025-11-11 17:32:30 +0100

    Update SoutExample.java

--- src/Java_05/SoutExample.java
+++ src/Java_05/SoutExample.java
@@ -7,5 +7,13 @@ public class SoutExample {
         System.out.println("5" + 2); // i bashkon per shkak se dy katetogi te ndryshe
         System.out.println(5 + "2"); // njejt
         System.out.println("" + 5 + 2); // int plus string = string plus int = string dmth njejt
+        System.out.println("" + (5 + 2)); // 7
+        System.out.println(5 + 2 + ""); // 7
+        System.out.println(5 - 2); // 3
+        System.out.println(5 * 2); // 10
+        System.out.println(5 / 2); // 2
+        System.out.println(5.0 / 2); // 2.5
+        System.out.println(5 % 2); // 1
+        System.out.println(5.5 / 2); // 2.75
     }
 }
