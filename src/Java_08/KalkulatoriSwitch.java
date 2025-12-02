de9e2013cc8e545a03e3610dc4376eddbeba76a4
Uvejs Gjelaj <61552503+UvejsGj@users.noreply.github.com>
2025-12-02 19:16:56 +0100

    m

---
+++ src/Java_08/KalkulatoriSwitch.java
@@ -0,0 +1,41 @@
+package Java_08;
+
+import java.util.Scanner;
+
+public class KalkulatoriSwitch {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Shkruani numrin e pare: ");
+        int nr1 = sc.nextInt();
+
+        System.out.print("Shkruani numrin e dyte: ");
+        int nr2 = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Shkruani operatorin (+,-,*,/,%): ");
+        char operatori = sc.nextLine().charAt(0);
+
+        switch (operatori) {
+            case '+' -> System.out.println("Shuma: " + (nr1 + nr2));
+            case '-' -> System.out.println("Shuma: " + (nr1 - nr2));
+            case '*' -> System.out.println("Shuma: " + (nr1 * nr2));
+            case '/' -> {
+                if (nr2 == 0) {
+                    System.out.println("Nuk lejohet pjestimi me 0");
+                } else {
+                    System.out.println("Heresi: " + (nr2 / nr1));
+                }
+            }
+            case '%' -> {
+                if (nr2 != 0) {
+                    System.out.println("Mbetja: " + (nr1 % nr2));
+                } else  {
+                    System.out.println("Nuk lejohet pjestimi me 0");
+                }
+            }
+            default -> System.out.println("Operatori gabim");
+        }
+    }
+
+}
