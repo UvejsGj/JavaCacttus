09ca74119e3fbd3cc82d30bd766fa0a43c05f8c6
Uvejs Gjelaj <61552503+UvejsGj@users.noreply.github.com>
2026-01-13 17:49:08 +0100

    Create Array.java

---
+++ src/Java_13/Array.java
@@ -0,0 +1,19 @@
+void main() {
+    int numri1 = 1;
+    int numri2 = 2;
+    int numri3 = 3;
+
+    // Scanner sc = new Scanner(System.in);
+    // Array => varg
+
+    int[] numrat; // vargu i tipit int
+    // inicializimi ne heap
+    // rezervimi i hapesires per ruajtjen e 3 numrave
+    // tipet primitive vlera fillestare (vlera default) 0 => [0, 0, 0]
+    // perveq boolean false => [false, false]
+    // ndersa tiper primitive vleren default e kane null => [null, null]
+    numrat = new int[3];
+    byte[] byteArray = new byte[5];
+    String[] stringArray = new String[10];
+
+}
